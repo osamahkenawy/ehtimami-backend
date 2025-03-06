fa6d623935f9678a35c1c1dede2d9048b2adc04c
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2025-03-06 14:10:10 +0400

    [11-uploading-files-service-with-aws-s3-final] Config FIle FOr AWS

---
+++ src/config/aws.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+const { S3Client } = require("@aws-sdk/client-s3");
+
+dotenv.config();
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION, // ✅ Ensure this is correct
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+module.exports = s3Client;
