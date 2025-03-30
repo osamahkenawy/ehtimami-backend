677bb60e58e0e8d0545755b763dc9e7d19f5520d
Osama Alaaeldin <osama.alaaeldin@wiot360.com>
2025-03-30 12:50:57 +0400

    [29-users-services] adding is verified logic into the system

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("@config/config");
 
+
 const authenticate = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
 
@@ -10,12 +11,19 @@ const authenticate = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded; // Store user data in request
+
+        if (!decoded.is_verified) {
+            return res.status(403).json({ error: "Account is not verified. Please contact the administrator." });
+        }
+
+        req.user = decoded; // Attach decoded user object
         next();
     } catch (error) {
         return res.status(403).json({ error: "Invalid or expired token." });
     }
-
 };
+
+module.exports = { authenticate };
+
 
 module.exports = { authenticate };
